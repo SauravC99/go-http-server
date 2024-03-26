1ca6020a04610e52fc19da32fee0da536084e196
Saurav <sauravc41@gmail.com>
2024-03-26 00:15:47 -0700

    parse http headers and test response with body

--- app/server.go
+++ app/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // headers section \r\n\r\n body section
@@ -35,17 +37,41 @@ func main() {
 
 	fmt.Println("Read incoming data:\n", string(recievedData))
 
-	x := 0
+	request := strings.Split(string(recievedData), "\r\n")
+
+	start_line := strings.Split(request[0], " ")
+
+	http_method := start_line[0]
+	path := start_line[1]
+
+	fmt.Println(request)
+	fmt.Println(start_line)
+	fmt.Println(http_method)
+	fmt.Println(path)
 
 	// need 2 sets of \r\n for end of headers section
-	if x == 1 {
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if path == "/" {
+		response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"
+		_, err = conn.Write([]byte(response))
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			os.Exit(1)
+		}
+	} else if strings.HasPrefix(path, "/echo/") {
+		//echo the request in body
+		response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n"
+		content := strings.TrimPrefix(path, "/echo/")
+		length := "Content-Length: " + strconv.Itoa(len(content)) + "\r\n\r\n"
+
+		response = response + length + content
+		_, err = conn.Write([]byte(response))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
 			os.Exit(1)
 		}
 	} else {
-		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		response := "HTTP/1.1 404 Not Found\r\n\r\n"
+		_, err = conn.Write([]byte(response))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
 			os.Exit(1)
