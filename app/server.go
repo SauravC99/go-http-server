bfda9c20a778e4e8c13a6af164e47cc2b6b7e0d6
Saurav <sauravc41@gmail.com>
2024-03-25 19:27:50 -0700

    recieve request and write to connection

--- app/server.go
+++ app/server.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+// headers section \r\n\r\n body section
+
 func main() {
 	fmt.Println("Server started")
 
@@ -22,5 +24,23 @@ func main() {
 	}
 
 	fmt.Println("Connection accept")
+
+	recievedData := make([]byte, 1024)
+
+	_, err = conn.Read(recievedData)
+	if err != nil {
+		fmt.Println("Error reading data: ", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Read incoming data:\n", string(recievedData))
+
+	// need 2 sets of \r\n for end of headers section
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
+
 	conn.Close()
 }
